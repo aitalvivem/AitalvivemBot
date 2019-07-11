cdad1338b4dca7d1c55ff04938a7a8dcdf48167c
aitalvivem <52562995+aitalvivem@users.noreply.github.com>
2019-07-11 10:59:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,24 +3,23 @@ import csv
 import pywikibot
 import functionsmain.APIfunction
 
-# i set the path of the .csv file with the lexicographical datas
+# I set the path of the .csv file with the lexicographical data and get the file
 fname = #PATH
-# i get the file
 file = open(fname, 'rt', encoding='utf-8')
 
-# i take the files datas using csv.reader and store it in 'lecteur'
+# I take the files datas using csv.reader and store it in 'lecteur'
 try:
-	lecteur = csv.reader(file, delimiter="§") 
-	# for row in lecteur:
-		# i get the id of the lexeme (if the lexeme don't exists, i create it)
-		# idLex = functionmain.APIfunction.chercheLex(row[2], row[5])
+	lecteur = csv.reader(file, delimiter="§")
+	
+	for row in lecteur:
+		# I get the id of the lexeme (if the lexeme don't exists, i create it)
+		idLex = functionmain.APIfunction.chercheLex(row[1], row[4], row[3], row[2])
                 
-		# if row[1] != row[2]
+		# if the form is different from the lemma
+		if row[0] != row[1]
 			# add the form
-			# functionmain.APIfunction.createForm(idLex, row[1], rox[3])
+			functionmain.APIfunction.createForm(idLex, row[1], rox[3], row[5])
 
 finally:
 	# close the .csv file
 	file.close()
-	
-
