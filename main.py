1651fd61c42ae1b16cac66dd94e771b284904b75
aitalvivem <52562995+aitalvivem@users.noreply.github.com>
2019-07-18 12:35:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,74 @@ import pywikibot
 import functionsmain.APIfunction
 
 # I set the path of the .csv file with the lexicographical data and get the file
-fname = #PATH
+fname = # PATH
 file = open(fname, 'rt', encoding='utf-8')
+
 
 # I take the files datas using csv.reader and store it in 'lecteur'
 try:
-	lecteur = csv.reader(file, delimiter="§")
+	lecteur = csv.reader(file, delimiter=";")
 	
 	for row in lecteur:
-		# I get the id of the lexeme (if the lexeme don't exists, i create it)
-		idLex = functionmain.APIfunction.chercheLex(row[1], row[4], row[3], row[2])
-                
-		# if the form is different from the lemma
-		if row[0] != row[1]
-			# add the form
-			functionmain.APIfunction.createForm(idLex, row[1], rox[3], row[5])
+		form = row[0]
+		lemme = row[1]
+		catGram = row[2]
+		catLex = row[3]
+		lg = row[4]
+		
+		# I get the id of the lexeme (if the lexeme don't exists, i create it) and then i get the data of the lexeme
+		idLex = functionsmain.APIfunction.chercheLex(lemme, lg, catLex, catGram)
+		infoLex = functionsmain.APIfunction.getLex(idLex)
+		
+		# I get the grammaticals category of the form
+		catGramW = functionsmain.APIfunction.detailCat(catGram)
+		
+		formExists = False
+		formeMatch = []
+		
+		# I check if the form already exists
+		for formes in infoLex['formes']:
+			if form == formes['representation']:
+				formExists = True
+				formeMatch += [formes]
+
+		if formExists == True:
+			memeCat = False
+			for forme in formeMatch:
+				if catGramW == forme['catGram']:
+					memeCat = True
+					formeMatch = forme
+					break
+
+			# if the form exists with the same grammatical category I check the dialect, if the dialecte don't exists I add it to the form
+			if memeCat == True:
+				if lg != 'fr':
+					if lg == 'lang':
+						idDial = 'Q65529243'
+					elif lg == 'gasc':
+						idDial = 'Q191085'
+					elif lg == 'prov':
+						idDial = 'Q101081'
+					elif lg == 'auv':
+						idDial = 'Q1152'
+					elif lg == 'lim':
+						idDial = 'Q65530372'
+					elif lg == 'viva':
+						idDial = 'Q65530697'
+					
+					if idDial not in formeMatch['dialectes']:
+						functionsmain.APIfunction.setDial(idForm, lg)
+						
+			else:
+				# else I create a new form
+				functionsmain.APIfunction.createForm(idLex, form, catGram, lg)
+				
+		# else I create a new form		
+		else:
+			if form != lemme:
+				functionsmain.APIfunction.createForm(idLex, form, catGram, lg)
 
 finally:
-	# close the .csv file
+	# on referme le fichier
 	file.close()
+
