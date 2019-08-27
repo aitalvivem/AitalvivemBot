ebdc247bb3d8b47d7f3dcde80dda5dec42d237f5
Michael F. Schönitzer <michael@schoenitzer.de>
2019-08-28 00:35:04 +0200

    Improve example

--- example.py
+++ example.py
@@ -20,3 +20,17 @@ print(S1.claims.keys())
 
 # Find or create a Lexeme by lemma, language and grammatical form
 L2 = LexData.get_or_create_lexeme(repo, "first", en, "Q1084")
+
+# You can easily create forms…
+if len(L2.forms) == 0:
+    L2.createForm("firsts", "Q146786")
+
+# …or senses, with or without additional claims
+if len(L2.senses) == 0:
+    L2.createSense(
+        {
+            "en": "Element in an ordered list which comes before all others according to the ordering",
+            "de": "einer Ordnung folgend das Element vor allen anderen",
+        },
+        claims={"P5137": ["Q19269277"]},
+    )
